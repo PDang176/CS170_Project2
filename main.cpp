f1d4423aec8b013c7628a431653b1d04a04246a7
Patrick Dang <patrickdangt@gmail.com>
2021-12-09 18:25:46 -0800

    Completed forward selection

--- main.cpp
+++ main.cpp
@@ -5,8 +5,41 @@
 #include <sstream>
 #include <cmath>
 #include <cfloat>
+#include <unordered_set>
 
 using namespace std;
+
+void forward_selection(vector<vector<double>> const & data){
+    vector<int> features(data[0].size(), 0); // Current features tested
+    vector<int> best_features = {features.begin(), features.end()};
+    double best_accuracy = 0;
+
+    for(int i = 1; i < data[0].size(); i++){ // Loop through all features
+        int feature_to_add;
+        double best_new_accuracy = 0;
+
+        for(int j = 1; j < data[0].size(); j++){
+            if(features[j] == 0){
+                double accuracy = nearest_neighbor_accuracy(data);
+
+                if(accuracy > best_new_accuracy){
+                    best_new_accuracy = accuracy;
+                    feature_to_add = j;
+                }
+            }
+        }
+        features[feature_to_add] = 1; // Adding the selected feature;
+        
+        if(best_new_accuracy > best_accuracy){ // Setting the top accuracy
+            best_accuracy = best_new_accuracy;
+            best_features = {features.begin(), features.end()};
+        }
+    }
+}
+
+void backward_elimination(vector<vector<double>> const & data){
+    cout << "Backward" << endl;
+}
 
 double calculate_distance(vector<double> const & v1, vector<double> const & v2){ // Distance formula between v1 and v2
     double sum = 0;
@@ -72,16 +105,57 @@ void reading_data(string name, vector<vector<double>>& data){
     }
 }
 
+int select_data(){
+    int data;
+    cout << "Small Data or Large Data (Type 1 or 2 respectively): ";
+    cin >> data;
+    while(data != 1 && data != 2){
+        cout << "Invalid input. Small Data or Large Data (Type 1 or 2 respectively): ";
+        cin >> data;
+    }
+
+    return data;
+}
+
+void select_algorithm(vector<vector<double>> const & smallData, vector<vector<double>> const & largeData){
+    int data = select_data();
+    int algorithm;
+    cout << "Forward Selection or Backward Elimination (Type 1 or 2 respectively): ";
+    cin >> algorithm;
+    while(algorithm != 1 && algorithm != 2){
+        cout << "Invalid input. Forward Selection or Backward Elimination (Type 1 or 2 respectively): ";
+        cin >> algorithm;
+    }
+
+    int combined = data * 10 + algorithm;
+
+    switch(combined){
+        case 11:
+            forward_selection(smallData);
+            break;
+        case 12:
+            backward_elimination(smallData);
+            break;
+        case 21:
+            forward_selection(largeData);
+            break;
+        case 22:
+            backward_elimination(largeData);
+            break;
+        default:
+            cout << "Error: Data or Algorithm doe not exist." << endl;
+            break;
+    }
+}
+
 int main(){
-    vector<vector<double>> largeData;
     vector<vector<double>> smallData;
+    vector<vector<double>> largeData;
 
-    reading_data("Large_data.txt", largeData);
     reading_data("Small_data.txt", smallData);
+    reading_data("Large_data.txt", largeData);
 
-    double temp = nearest_neighbor_accuracy(smallData);
-
-    cout << temp << endl;
+    select_algorithm(smallData, largeData);
 
     return 0;
 }
